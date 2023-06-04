2af0303d12fd97677596072a8050621171308807
aichiaws2022 <117337037+aichiaws2022@users.noreply.github.com>
2023-06-04 21:14:04 +0900

    Update endpoint.tf

--- Terraform/endpoint.tf
+++ Terraform/endpoint.tf
@@ -44,50 +44,7 @@ resource "aws_vpc_endpoint" "ssmmessages" {
 
 }
 
-resource "aws_vpc_endpoint" "ssm2" {
-  vpc_id            = aws_vpc.vpc.id
-  subnet_ids        = [aws_subnet.private_subnet_1-1c.id]
-  service_name      = "com.amazonaws.ap-northeast-1.ssm"
-  vpc_endpoint_type = "Interface"
 
-
-  security_group_ids = [
-    aws_security_group.ssm.id
-  ]
-
-  private_dns_enabled = true
-
-}
-
-resource "aws_vpc_endpoint" "ec2messages2" {
-  vpc_id            = aws_vpc.vpc.id
-  subnet_ids        = [aws_subnet.private_subnet_1-1c.id]
-  service_name      = "com.amazonaws.ap-northeast-1.ec2messages"
-  vpc_endpoint_type = "Interface"
-
-
-  security_group_ids = [
-    aws_security_group.ssm.id,
-  ]
-
-  private_dns_enabled = true
-
-}
-
-resource "aws_vpc_endpoint" "ssmmessages2" {
-  vpc_id            = aws_vpc.vpc.id
-  subnet_ids        = [aws_subnet.private_subnet_1-1c.id]
-  service_name      = "com.amazonaws.ap-northeast-1.ssmmessages"
-  vpc_endpoint_type = "Interface"
-
-
-  security_group_ids = [
-    aws_security_group.ssm.id,
-  ]
-
-  private_dns_enabled = true
-
-}
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.vpc.id
